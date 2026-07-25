6b2982540a985aeb574b07d94058b743094b0a0a
Nikolay Yurchenko <nikolay@yurchenko.su>
2026-07-25 14:00:15 +0300

    Adding missing @Override annotations (part 3). Adding RRA/RWA/RUI annotations. Some localization and refactoring (#13)
    * Adding missing @Override annotations (part 3). Adding RRA/RWA/RUI annotations. Some localization and refactoring.
    
    * Fixes.
    
    * Fixing compilation of SpringBeanIdConverter.

--- plugin/src/main/java/com/intellij/spring/impl/ide/model/xml/beans/DefaultAutowire.java
+++ plugin/src/main/java/com/intellij/spring/impl/ide/model/xml/beans/DefaultAutowire.java
@@ -1,6 +1,5 @@
 // Generated on Thu Nov 09 17:15:14 MSK 2006
 // DTD/Schema  :    http://www.springframework.org/schema/beans
-
 package com.intellij.spring.impl.ide.model.xml.beans;
 
 import consulo.xml.dom.NamedEnum;
@@ -10,7 +9,6 @@ import org.jetbrains.annotations.NonNls;
  * http://www.springframework.org/schema/beans:default-autowireAttrType enumeration.
  */
 public enum DefaultAutowire implements NamedEnum {
-
     AUTODETECT ("autodetect"),
     BY_NAME ("byName"),
     BY_TYPE ("byType"),
